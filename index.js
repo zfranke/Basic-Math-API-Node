7736abe7f895ee81da70a9896dfa6fd9aad305ed
zfranke <zfranke@live.com>
2022-02-02 12:25:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,28 @@ app.get('/', (req, res) => {
 
 //post request for addition of two numbers
 app.post('/add', (req, res) => {
-    const {num1, num2} = req.body;
-    const result = num1 + num2;
-    res.send(`<h1>${result}</h1>`)
+    const { num1, num2 } = req.body;
+    const result = parseInt(num1) + parseInt(num2);
+    res.send(`<h1>Result of Addition: </h1> <p>${num1} + ${num2} = ${result}</p>`);
 });
 
+//Subtraction of two numbers
+app.post('/subtract', (req, res) => {
+    const { num1, num2 } = req.body;
+    const result = parseInt(num1) - parseInt(num2);
+    res.send(`<h1>Result of Subtraction: </h1> <p>${num1} - ${num2} = ${result}</p>`);
+});
+
+//Multiplication of two numbers
+app.post('/multiply', (req, res) => {
+    const { num1, num2 } = req.body;
+    const result = parseInt(num1) * parseInt(num2);
+    res.send(`<h1>Result of Multiplication: </h1> <p>${num1} * ${num2} = ${result}</p>`);
+});
+
+//Division of two numbers
+app.post('/divide', (req, res) => {
+    const { num1, num2 } = req.body;
+    const result = parseInt(num1) / parseInt(num2);
+    res.send(`<h1>Result of Division: </h1> <p>${num1} / ${num2} = ${result}</p>`);
+});
